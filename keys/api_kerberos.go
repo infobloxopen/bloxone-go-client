efc547f8f94bfbe16d63039afdf0041d4766cad7
Ashish Mathew <mathewa@infoblox.com>
2024-02-12 14:32:52 -0800

    Add go fmt to build actions (#19)
    * Add go fmt to build actions
    
    * run go fmt
    
    * remove mac and windows from OS actions

--- keys/api_kerberos.go
+++ keys/api_kerberos.go
@@ -24,14 +24,14 @@ import (
 type KerberosAPI interface {
 
 	/*
-		KerberosDelete Delete the Kerberos key.
+			KerberosDelete Delete the Kerberos key.
 
-		Use this method to delete a __KerberosKey__ object.
-	A __KerberosKey__ object represents a Kerberos key.
+			Use this method to delete a __KerberosKey__ object.
+		A __KerberosKey__ object represents a Kerberos key.
 
-		@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-		@param id An application specific resource identity of a resource
-		@return ApiKerberosDeleteRequest
+			@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+			@param id An application specific resource identity of a resource
+			@return ApiKerberosDeleteRequest
 	*/
 	KerberosDelete(ctx context.Context, id string) ApiKerberosDeleteRequest
 
@@ -39,13 +39,13 @@ type KerberosAPI interface {
 	KerberosDeleteExecute(r ApiKerberosDeleteRequest) (*http.Response, error)
 
 	/*
-		KerberosList Retrieve Kerberos keys.
+			KerberosList Retrieve Kerberos keys.
 
-		Use this method to retrieve __KerberosKey__ objects.
-	A __KerberosKey__ object represents a Kerberos key.
+			Use this method to retrieve __KerberosKey__ objects.
+		A __KerberosKey__ object represents a Kerberos key.
 
-		@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-		@return ApiKerberosListRequest
+			@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+			@return ApiKerberosListRequest
 	*/
 	KerberosList(ctx context.Context) ApiKerberosListRequest
 
@@ -54,14 +54,14 @@ type KerberosAPI interface {
 	KerberosListExecute(r ApiKerberosListRequest) (*KeysListKerberosKeyResponse, *http.Response, error)
 
 	/*
-		KerberosRead Retrieve the Kerberos key.
+			KerberosRead Retrieve the Kerberos key.
 
-		Use this method to retrieve a __KerberosKey__ object.
-	A __KerberosKey__ object represents a Kerberos key.
+			Use this method to retrieve a __KerberosKey__ object.
+		A __KerberosKey__ object represents a Kerberos key.
 
-		@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-		@param id An application specific resource identity of a resource
-		@return ApiKerberosReadRequest
+			@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+			@param id An application specific resource identity of a resource
+			@return ApiKerberosReadRequest
 	*/
 	KerberosRead(ctx context.Context, id string) ApiKerberosReadRequest
 
@@ -70,14 +70,14 @@ type KerberosAPI interface {
 	KerberosReadExecute(r ApiKerberosReadRequest) (*KeysReadKerberosKeyResponse, *http.Response, error)
 
 	/*
-		KerberosUpdate Update the Kerberos key.
+			KerberosUpdate Update the Kerberos key.
 
-		Use this method to update a __KerberosKey__ object.
-	A __KerberosKey__ object represents a Kerberos key.
+			Use this method to update a __KerberosKey__ object.
+		A __KerberosKey__ object represents a Kerberos key.
 
-		@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
-		@param id An application specific resource identity of a resource
-		@return ApiKerberosUpdateRequest
+			@param ctx context.Context - for authentication, logging, cancellation, deadlines, tracing, etc. Passed from http.Request or context.Background().
+			@param id An application specific resource identity of a resource
+			@return ApiKerberosUpdateRequest
 	*/
 	KerberosUpdate(ctx context.Context, id string) ApiKerberosUpdateRequest
 
